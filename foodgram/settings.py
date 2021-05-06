0913595501fddd9433502e3f49a15c8aa2dba49f
Alexey Sukhachevskiy <alyoshasu@192.168.1.13>
2021-05-06 22:51:03 +0300

    flake8 check

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -76,18 +76,18 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME':
+         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+     },
+    {'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
+     },
+    {'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
+     },
+    {'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
+     },
 ]
 
 LANGUAGE_CODE = 'ru'
@@ -123,9 +123,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 
-    # это ломает JS при создании рецепта, так что оставлю как есть
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 50
+# это ломает JS при создании рецепта, так что оставлю как есть
+# 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+# 'PAGE_SIZE': 50
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
